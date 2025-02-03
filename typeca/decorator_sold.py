ad3696f493a933b5a420d24291ea7a7133f9017d
George <georgiiviktorov@MacBook-Pro.local>
2025-02-03 12:17:51 +0400

    DIP ISP

--- typeca/decorator_sold.py
+++ typeca/decorator_sold.py
@@ -1,9 +1,31 @@
 from abc import ABC, abstractmethod
-from typing import Type
+from typing import Any, Type, get_args
 
 
 class TypeChecker(ABC):
     @abstractmethod
-    def check_type(self, expected_type: Type) -> bool:
+    def check_type(self, value: Any, expected_type: Type) -> bool:
         pass
 
+
+class TypeCheckerFactory(ABC):
+    @abstractmethod
+    def get_checker(self, expected_type: Type) -> TypeChecker:
+        pass
+
+
+class StandardTypeChecker(TypeChecker):
+    def check_type(self, value: Any, expected_type: Type) -> bool:
+        return isinstance(value, expected_type)
+
+
+class ListChecker(TypeChecker):
+    def __init__(self, factory: TypeCheckerFactory):
+        self.factory = factory
+
+    def check_type(self, value: Any, expected_type: Type) -> bool:
+        if not isinstance(value, list):
+            return False
+        elem_type = get_args(expected_type)[0]
+        checker = self.factory.get_checker(elem_type)
+        return all(checker.check_type(value, elem_type) for value in value)
